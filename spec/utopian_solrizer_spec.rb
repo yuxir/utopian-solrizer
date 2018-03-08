79018d42246bfa30a3c861baa24daa82d637590f
yuxi <me@yuxifan.com>
2018-03-09 00:17:32 +0100

    update spec tests

--- spec/utopian_solrizer_spec.rb
+++ spec/utopian_solrizer_spec.rb
@@ -1,18 +1,26 @@
 require 'spec_helper'
 
 describe UtopianSolrizer do
-  it "has solrizer_post" do
-#    post = UtopianRuby::UtopianRubyAPI.get_post_obj('espoem','old-moderated-posts-are-received-and-shown-instead-of-the-recent-one')
+  @@solr_options = { read_timeout: 120, open_timeout: 120, url: 'http://localhost:8983/solr/utopian' }
+
+  it "has solrize_post" do
     post = UtopianRuby::UtopianRubyAPI.get_post_obj('yuxi','utopian-api-ruby-client')
-    solr_options = { read_timeout: 120, open_timeout: 120, url: 'http://localhost:8983/solr/utopian' }
-    response = UtopianSolrizer.solrize_post(post, solr_options)
-    puts response
+    response = UtopianSolrizer.solrize_post(post, @@solr_options)
+    expect(response['responseHeader']['status']).to eq(0)
   end
 
   it "has query" do
-#    solr_options = { read_timeout: 120, open_timeout: 120, url: 'http://localhost:8983/solr/utopian' }
-#    params = { :q => "espoem", :qf => "author_tesim" }
-#    response = UtopianSolrizer.query(solr_options, params)
-#    puts response
+    params = { :q => "*:*" }
+    response = UtopianSolrizer.query(@@solr_options, params)
+    expect(response['responseHeader']['status']).to eq(0)
+    #response["response"]["docs"].each do |d|
+    #  puts d["id"]
+    #  puts d["author"]
+    #end
+  end
+
+  it "has exist method" do
+    id = '0'
+    expect(UtopianSolrizer.exist(@@solr_options, id)).to eq(false)
   end
 end
