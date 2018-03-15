46046ddf70999593c5a0b67861ac9f3bb86dc383
yuxi <me@yuxifan.com>
2018-03-16 00:25:20 +0100

    support sold_fields parameter to allow user decide which fields to be indexed

--- lib/utopian_solrizer.rb
+++ lib/utopian_solrizer.rb
@@ -7,7 +7,7 @@ module UtopianSolrizer
   
   class << self
     # define utopian solr fileds by following solr dynamic fields conventions
-    @@default_fields = {
+    @@utopian_solr_field_mapping = {
       'id'         => 'id',
       'author'     => 'author_ssi',
       'moderator'  => 'moderator_ssim',
@@ -18,43 +18,91 @@ module UtopianSolrizer
       'body'       => 'body_tsim',
       'created'    => 'created_dts',
       'repository' => 'repository_ssi'
+
+
+  
     }
 
+    # set default utopian fields to index
+    @@default_fields = ['author',
+                        'moderator',
+                        'permlink',
+                        'category',
+                        'tags',
+                        'title',
+                        'body',
+                        'created',
+                        'repository'
+                        ]
     
     # Add/update a post in solr
-    def solrize_post(post, solr_options, overwrite=true)
+    def solrize_post(post, solr_options, solr_fields=nil)
       rsolr = RSolr.connect solr_options
-      if (overwrite==true) or (not exist(solr_options, post.id))
-        rsolr.add(
-          @@fields['id']         => post.id,
-          @@fields['author']     => post.author,
-          @@fields['moderator']  => post.moderator,
-          @@fields['permlink']   => post.permlink,
-          @@fields['category']   => post.json_metadata['type'],
-          @@fields['tags']       => post.json_metadata['tags'],
-          @@fields['title']      => post.title,
-          @@fields['body']       => post.body,
-          @@fields['created']    => post.created,
-          @@fields['repository'] => post.json_metadata['repository']['html_url']
-        )
-        rsolr.commit
+      if solr_fields.nil? or solr_fields.length==0
+        solr_fields = @@default_fields
       end
+      solr_json = {}
+
+      solr_fields.each do |f|
+        solr_json[@@utopian_solr_field_mapping[f]] = post_value(post, f)  
+      end
+      rsolr.add solr_json
+      rsolr.commit
     end
 
-    def post_json(post)
-      {
-          @@fields['id']         => post.id,
-          @@fields['author']     => post.author,
-          @@fields['moderator']  => post.moderator,
-          @@fields['permlink']   => post.permlink,
-          @@fields['category']   => post.json_metadata['type'],
-          @@fields['tags']       => post.json_metadata['tags'],
-          @@fields['title']      => post.title,
-          @@fields['body']       => post.body,
-          @@fields['created']    => post.created,
-          @@fields['repository'] => post.json_metadata['repository']['html_url']
-      }
+    # return post value for a particular field_name
+    def post_value(post, field_name)
+      post.id                                      if field_name=='id'
+      post.author                                  if field_name=='author'
+      post.moderator                               if field_name=='moderator'
+      post.permlink                                if field_name=='permlink'
+      post.json_metadata['type']                   if field_name=='category'
+      post.json_metadata['tags']                   if field_name=='tags'
+      post.title                                   if field_name=='title'
+      post.body                                    if field_name=='body'
+      post.created                                 if field_name=='created'
+      post.json_metadata['repository']['html_url'] if field_name=='repository'
+      post.flagged                                 if field_name=='flagged'
+      post.reviewed                                if field_name=='reviewed'
+      post.pending                                 if field_name=='pending'
+      post.last_update                             if field_name=='last_update'
+      post.active                                  if field_name=='active'
     end
+
+    # Add/update a post in solr
+ #   def solrize_post(post, solr_options, overwrite=true)
+ #     rsolr = RSolr.connect solr_options
+ #     if (overwrite==true) or (not exist(solr_options, post.id))
+ #       rsolr.add(
+ #         @@fields['id']         => post.id,
+ #         @@fields['author']     => post.author,
+ #         @@fields['moderator']  => post.moderator,
+ #         @@fields['permlink']   => post.permlink,
+ #         @@fields['category']   => post.json_metadata['type'],
+ #         @@fields['tags']       => post.json_metadata['tags'],
+ #         @@fields['title']      => post.title,
+ #         @@fields['body']       => post.body,
+ #         @@fields['created']    => post.created,
+ #         @@fields['repository'] => post.json_metadata['repository']['html_url']
+ #       )
+ #       rsolr.commit
+ #     end
+ #   end
+
+ #   def post_json(post)
+ #     {
+ #         @@fields['id']         => post.id,
+ #         @@fields['author']     => post.author,
+ #         @@fields['moderator']  => post.moderator,
+ #         @@fields['permlink']   => post.permlink,
+ #         @@fields['category']   => post.json_metadata['type'],
+ #         @@fields['tags']       => post.json_metadata['tags'],
+ #         @@fields['title']      => post.title,
+ #         @@fields['body']       => post.body,
+ #         @@fields['created']    => post.created,
+ #         @@fields['repository'] => post.json_metadata['repository']['html_url']
+ #     }
+ #   end
 
     # solrize from json
     def solrize_from_json(json_documents, solr_options)
